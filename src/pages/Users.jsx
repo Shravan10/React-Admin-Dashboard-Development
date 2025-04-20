bcfa03f404f7f4bbbcb827b1512fbda845fc9155
Shravan10 <shravankumarghanchi@gmail.com>
2025-04-20 17:41:16 +0530

    Developed users page statecard

--- src/pages/Users.jsx
+++ src/pages/Users.jsx
@@ -2,9 +2,11 @@ import React from 'react'
 // eslint-disable-next-line no-unused-vars
 import {motion} from "framer-motion";
 import Header from '../component/common/Header';
-import ProductTable from '../component/products/ProductTable';
+import UsersTable from '../userss/UsersTable';
 import SalesTrendChart from '../component/products/SalesTrendChart';
 import CategoryDistributionChart from '../overview/CateogoryDistributionChart';
+import { UserCheck, UserIcon, UserPlus, UserX } from 'lucide-react';
+import StateCard from '../component/common/StateCard';
 
 const Users = () => {
   return (
@@ -12,17 +14,11 @@ const Users = () => {
     <Header title="Users"/>
     <main className="max-w-7xl mx-auto py-6 px-4 lg:px-8">
        <motion.div className="grid grid-cols-1 gap-5 sm:grid-cols-2 lg:grid-cols-4 mb-8" initial={{opacity:0,y:20}} animate={{opacity:1,y:0}} transition={{duration:1}}>
-          {/* <StateCard name="Total Product" icon={Package} value='$12,345' color='#6366F1'/>
-          <StateCard name="New Users" icon={TrendingUp} value="567" color="#8B5CF6"/>
-          <StateCard name="Total Selling" icon={AlertTriangle} value="1234" color="#EC4899"/>
-          <StateCard name="Low Stock" icon={DollarSign} value="12.5%" color="#10B981"/> */}
+          <StateCard name="Total Users" icon={UserIcon} value='52,845' color='#6366F1'/>
+          <StateCard name="Active Users" icon={UserCheck} value="243" color="#F59E0B"/>
+          <StateCard name="New Users Today" icon={UserPlus} value="98520" color="#10B981"/>
+          <StateCard name="Churn Rate" icon={UserX} value="2.4%" color="#EF4444"/>
        </motion.div>
-       {/* Charts */}
-       <ProductTable/>
-       <div className="grid grid-cols-1 lg:grid-cols-2 gap-8">
-        <SalesTrendChart/>
-        <CategoryDistributionChart/>
-      </div>
       </main>
 
   </div>
---
+++ src/userss/UsersTable.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const UsersTable = () => {
+  return (
+    <>
+      <h1>Users Table</h1>
+    </>
+  );
+};
+
+export default UsersTable;
