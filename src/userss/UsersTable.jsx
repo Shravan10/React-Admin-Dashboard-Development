53bf19bcca76f15c70e35bed3c902b017e55f54a
Shravan10 <shravankumarghanchi@gmail.com>
2025-04-20 20:31:42 +0530

    developed search box with search icon

--- src/userss/UsersTable.jsx
+++ src/userss/UsersTable.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 // eslint-disable-next-line no-unused-vars
 import {motion} from "framer-motion"
+import { Search } from "lucide-react";
 
 const UsersTable = () => {
   return (
@@ -10,7 +11,13 @@ const UsersTable = () => {
       animate={{opacity:1,y:0}}
       transition={{delay:0.2}}
       >
-
+      <div className="flex justify-between items-center mb-6">
+      <h2 className="text-xl font-semibold text-gray-100">Users</h2>
+      <div className="relative">
+        <input type="text" placeholder="Search Users..." className="bg-gray-700 text-white placeholder-gray-400 rounded-lg pl-10 pr-4 py-2 focus:outline-none focus:ring-2 focus:ring-blue:500"/>
+        <Search className="absolute left-3 top-2.5 text-gray-400" size={18} />
+      </div>
+      </div>
       </motion.div>
     </>
   );
