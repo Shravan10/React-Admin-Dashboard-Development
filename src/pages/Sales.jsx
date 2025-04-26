04ffcd0bd23efbf219a032b366b61742b82a1899
Shravan10 <shravankumarghanchi@gmail.com>
2025-04-26 22:14:22 +0530

    Remove dashboard title from header component

--- src/pages/Sales.jsx
+++ src/pages/Sales.jsx
@@ -11,7 +11,7 @@ import { DailySalesTrend } from '../component/sales/DailySalesTrend';
 const Sales = () => {
   return (
     <div className="flex-1 overflow-auto relative z-10">
-     <Header title="Sales Dashboard"/>
+     <Header title="Sales"/>
      <main className="max-w-7xl mx-auto py-6 px-4 lg:px-8">
       {/* Sales Stats */}
       <motion.div className='grid grid-cols-1 gap-5 sm:gird-cols-2 lg:grid-cols-4 mb-8'
