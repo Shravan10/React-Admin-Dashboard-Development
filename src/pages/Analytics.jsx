8598683da722af674cd9c5515fd51eca38f64682
Shravan10 <shravankumarghanchi@gmail.com>
2025-04-27 20:08:56 +0530

    Developed state card for overview page

--- src/pages/Analytics.jsx
+++ src/pages/Analytics.jsx
@@ -1,10 +1,15 @@
 import React from 'react'
 import Header from '../component/common/Header'
+import OverviewCard from '../component/analytics/OverviewCard'
+import RevenueChart from '../component/analytics/RevenueChart'
 
 const Analytics = () => {
   return (
     <div className="flex-1 overflow-auto relative z-10 bg-gray-900">
       <Header title={"Analytics"}/>
+      <main className="max-w-7xl mx-auto py-6 px-4 lg:px-8">
+       <OverviewCard/>
+      </main>
     </div>
   )
 }
