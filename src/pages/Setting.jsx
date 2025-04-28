c6b429c4067114ed20ef96fa5f9c4f35e0b21bd5
Shravan10 <shravankumarghanchi@gmail.com>
2025-04-28 12:42:53 +0530

    Developed profile section for setting page

--- src/pages/Setting.jsx
+++ src/pages/Setting.jsx
@@ -1,8 +1,15 @@
 import React from 'react'
+import Header from '../component/common/Header'
+import Profile from '../component/settings/Profile'
 
 const Setting = () => {
   return (
-    <div>Setting</div>
+   <div className="flex-1 overflow-auto relative z-10 bg-gray-900">
+    <Header title="Setting"/>
+    <main className="max-w-4xl mx-auto py-6 px-4 lg:px-8">
+     <Profile/>
+    </main>
+   </div>
   )
 }
 
